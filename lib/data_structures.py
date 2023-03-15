8e0f62f5590c87064d4767c81526dd94b142d0b9
minyoungshin <mins6649@gmail.com>
2023-03-14 23:03:46 -0400

    data stucture

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,35 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [key["name"] for key in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [key for key in spicy_foods if key["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine_type = food["cuisine"]
+        heat = food["heat_level"] * "🌶"
+        print(f"{name} ({cuisine_type}) | Heat Level: {heat}")
+        
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    return [key for key in spicy_foods if key["cuisine"].lower() == cuisine.lower()][0]
+# HOW TO RETURN A SINGLE DICT?????
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine_type = food["cuisine"]
+        heat = food["heat_level"] * "🌶"
+        if food["heat_level"] > 5:
+            print(f"{name} ({cuisine_type}) | Heat Level: {heat}")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_levels = [key["heat_level"] for key in spicy_foods]
+    return (sum(heat_levels))/(len(spicy_foods))
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
